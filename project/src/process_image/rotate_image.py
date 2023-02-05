229fe123341373b0d3736d5b9eb57e4ddad53f30
MarceliG <marcel9817@gmail.com>
2023-02-05 16:50:33 +0100

    add feature rotate_image for readability

--- project/src/process_image/rotate_image.py
+++ project/src/process_image/rotate_image.py
@@ -1,26 +1,23 @@
-# from __future__ import division, print_function
+"""Automatically detect rotation and line spacing of an image of text using."""
+
 from skimage.transform import radon
-
-# from PIL import Image
-# from PIL.Image import Resampling
-import numpy
-from numpy.fft import rfft
-from numpy import asarray, mean, array, blackman, argmax
-
-# import matplotlib.pyplot as plt
-
-# from matplotlib.mlab import rms_flat
-# from matplotlib import mlab
-# import cv2
+from numpy import array, argmax
+import imutils
 
 
-def detect_angle():
-    pass
+def detect_angle(image):
+    # Do the radon transform and display the result
+    sinogram = radon(image)
+    # Find the RMS value of each row and find "busiest" rotation,
+    # where the transform is lined up perfectly with the alternating dark
+    # text and white lines
+    rms = lambda x_seq: (sum(x * x for x in x_seq) / len(x_seq)) ** (1 / 2)
+    r = array([rms(line) for line in sinogram.transpose()])
+    angle = argmax(r) - 90
+    return angle
 
 
-def rotate():
-    pass
-
-
-if __name__ == "__main__":
-    rotate()
+def rotate(image):
+    angle = detect_angle(image)
+    rotate_image = imutils.rotate_bound(image, angle)
+    return rotate_image
