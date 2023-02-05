28b6b65a60a54ca9bb234baf5c486a87fd64ae4b
MarceliG <marcel9817@gmail.com>
2023-02-05 18:46:53 +0100

    convert image to text

--- project/src/app.py
+++ project/src/app.py
@@ -1,18 +1,18 @@
 from operation import load_image
 from process_image import remove_shadow, rotate
 
+
 def main():
     # image = load_image(image_name="bill.jpg")
     # print(image.shape)
-    
+
     # image = remove_shadow("bill.jpg")
     # print(image)
-    
+
     # after_rotation = rotate(image)
     # print(after_rotation)
-    
+    pass
 
 
 if __name__ == "__main__":
     main()
-
