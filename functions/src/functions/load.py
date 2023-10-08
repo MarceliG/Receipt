b0f0f9722e9f82a481e09f6649ec008543c52ced
Marceli <57628878+MarceliG@users.noreply.github.com>
2023-10-08 11:44:47 +0200

    Functions (#7)
    * add const
    
    * refactor load_image
    
    * gitignore
    
    * add new photo

--- functions/src/functions/load.py
+++ functions/src/functions/load.py
@@ -1,48 +1,51 @@
-import os
+import logging
 from typing import Optional
 
 import cv2
 import numpy as np
 
-from functions.const import PHTOTOS_INPUT
+# from functions.const import PHTOTOS_INPUT
+logger = logging.getLogger(__name__)
 
 
-def load_original_image(image_name: str) -> Optional[np.ndarray]:
-    """Load original image.
+def load_image(image_path: str) -> Optional[np.ndarray]:
+    """Load image.
 
     Args:
-        image_name (str): image name from database.
+        image_path (str): image name from database.
 
     Returns:
-        image: return image by numpy array.
+        image: return image.
     """
-    image = cv2.imread(os.path.join(PHTOTOS_INPUT, image_name))
-    return image
+    try:
+        image = cv2.imread(image_path)
+        return image
+    except FileNotFoundError as error:
+        logger.error(error)
+        raise
 
-    # (h, w, c) = image.shape[:3]
+# # TODO chanege to load orginal image and second function loaad resize image
+# def load_image(
+#     image_name: str,
+#     dimension_resize: int = 800,
+#     interpolation: int = cv2.INTER_LANCZOS4,
+# ) -> Optional[np.ndarray]:
+#     path = os.path.realpath(os.path.join(PHTOTOS_INPUT, image_name))
+#     if os.path.exists(path):
+#         image = cv2.imread(path)
+#         height, width = image.shape[:2]
 
-# TODO chanege to load orginal image and second function loaad resize image
-def load_image(
-    image_name: str,
-    dimension_resize: int = 800,
-    interpolation: int = cv2.INTER_LANCZOS4,
-) -> Optional[np.ndarray]:
-    path = os.path.realpath(os.path.join(PHTOTOS_INPUT, image_name))
-    if os.path.exists(path):
-        image = cv2.imread(path)
-        height, width = image.shape[:2]
+#         if height > width:
+#             new_height = dimension_resize
+#             new_width = int(width * (dimension_resize / height))
+#         else:
+#             new_width = dimension_resize
+#             new_height = int(height * (dimension_resize / width))
 
-        if height > width:
-            new_height = dimension_resize
-            new_width = int(width * (dimension_resize / height))
-        else:
-            new_width = dimension_resize
-            new_height = int(height * (dimension_resize / width))
-
-        # Zeskaluj obraz
-        resized_image = cv2.resize(
-            image,
-            (new_width, new_height),
-            interpolation=interpolation,
-        )
-    return resized_image
+#         # Zeskaluj obraz
+#         resized_image = cv2.resize(
+#             image,
+#             (new_width, new_height),
+#             interpolation=interpolation,
+#         )
+#     return resized_image
